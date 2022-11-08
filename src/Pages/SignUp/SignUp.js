a3b7dd1f7bd5bd809caf874cb536cd1ed732a28d
saif-sudo <mdalisaif4@gmail.com>
2022-11-08 22:19:10 +0600

    email added

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 const SignUp = () => {
+
+  const {createUser} = useContext(AuthContext)
     const handleSignUp = event => {
         event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email,password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+        })
+        .catch(err => console.error(err));
     }
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -19,19 +32,19 @@ const SignUp = () => {
                 <label className="label">
                   <span className="label-text">Name</span>
                 </label>
-                <input type="text" name="name" placeholder="email" className="input input-bordered" />
+                <input type="text" name="name" placeholder="Name" className="input input-bordered" />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="text" placeholder="email"  className="input input-bordered" required/>
+                <input type="text" placeholder="email" name='email'  className="input input-bordered" required/>
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="text" placeholder="password" className="input input-bordered" required/>
+                <input type="text" placeholder="password" name='password' className="input input-bordered" required/>
                
               </div>
               <div className="form-control mt-6">
