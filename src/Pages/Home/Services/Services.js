1a7da8326fdde348b757038d9583afcb667c151f
saif-sudo <mdalisaif4@gmail.com>
2022-11-10 06:10:21 +0600

    limit added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -18,7 +18,7 @@ const Services = () => {
             </div>
             <div className='grid gap-6 grid-cold-1 md:grid-cols-2 lg:grid-cols-3'>
             {
-                    services.map(service => <ServiceCard
+                    services.slice(0,3).map(service => <ServiceCard
                         key={service._id}
                         service={service}
                     ></ServiceCard> )
